95bf6b5d93d9d56cb48ef26547aa00c911b61459
LucasAlAlam <lucasaars@dcc.ufrj.br>
2023-08-20 20:37:31 +0000

    bugfix: retirada parâmeros invalidos no validation

--- src/models/Contato.js
+++ src/models/Contato.js
@@ -7,21 +7,18 @@ const Contato = sequelize.define('Contato', {
         defaultValue: 55,
         validate: {
             isInt: true,
-            msg: 'Insira um número para o código de país'
         }
     },
     codArea:{
         type: DataTypes.INTEGER,
         validate: {
             isInt: true,
-            msg: 'Insira um número para o código de área'
         }
     },
     numero:{
         type: DataTypes.STRING,
         validate: {
             isNumeric: true,
-            msg: 'Insira apenas números para a informação de número de telefone'
         }
     }
 });
