306095de4300ea6d4560eba4a342f6dbcccde5b3
LucasAlAlam <lucasaars@dcc.ufrj.br>
2023-08-20 19:21:13 +0000

    bugfix: retirada parâmetros invalidos do validator

--- src/models/Produto.js
+++ src/models/Produto.js
@@ -2,32 +2,16 @@ const DataTypes = require('sequelize');
 const sequelize = require('../config/sequelize');
 
 const Produto = sequelize.define('Produto', {
-    // idProduto: {
-    //     type: DataTypes.INTEGER,
-    //     primaryKey: true,
-    //     autoIncrement: true,
-    //     unique: true,
-    // },
     imagem:{
         type: DataTypes.STRING,
     },
     nome: {
         type: DataTypes.STRING,
         allowNull: false,
-        validate:{
-            isNull: {
-                msg: 'Insira um nome para o produto'
-            }
-        }
     },
     valor: {
         type: DataTypes.FLOAT,
         allowNull: false,
-        validate:{
-            isNull: {
-                msg: 'Insira um valor para o produto'
-            }
-        }
     },
     quantidadeEstoque: {
         type: DataTypes.INTEGER,
