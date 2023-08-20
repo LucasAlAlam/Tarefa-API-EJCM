3032b9a5fe6346c3047cfce7d98809f64d6a65b0
LucasAlAlam <lucasaars@dcc.ufrj.br>
2023-08-20 19:19:56 +0000

    feat: add rotas para produto

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,4 +9,12 @@ router.get('/usuario/:id', usuarioController.show);
 router.put('/usuario/:id', usuarioController.update);
 router.delete('/usuario/:id', usuarioController.destroy);
 
+const produtoController = require ('../controllers/ProdutoController')
+
+router.post('/produto', produtoController.create);
+router.get('/produto', produtoController.index);
+router.get('/produto/:id', produtoController.show);
+router.put('/produto/:id', produtoController.update);
+router.delete('/produto/:id', produtoController.destroy);
+
 module.exports = router;
