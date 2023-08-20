2a6d2d71dfd3080c5a5a50438d1cd20555e550ea
Lucas Al Alam <lucasalalam@protonmail.com>
2023-08-20 00:35:03 -0300

    bugfix: correção de nome em routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,6 +7,6 @@ router.post('/usuario', usuarioController.create);
 router.get('/usuario', usuarioController.index);
 router.get('/usuario/:id', usuarioController.show);
 router.put('/usuario', usuarioController.update);
-router.destroy('/usuario', usuarioController.destroy);
+router.delete('/usuario', usuarioController.destroy);
 
 module.exports = router;
