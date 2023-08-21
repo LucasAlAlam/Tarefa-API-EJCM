435126256631e92d691fb45ff61fe29499eb4472
LucasAlAlam <lucasaars@dcc.ufrj.br>
2023-08-21 02:56:30 +0000

    feat: controller do produto

--- src/controllers/ProdutoController.js
+++ src/controllers/ProdutoController.js
@@ -1,16 +1,17 @@
-const modelProduto = require('../models/Produto');
+const Produto = require('../models/Produto');
+const Vendedor = require('../models/Vendedor');
 
 async function create (req, res) {
     try {
-        const produto = await modelProduto.create(req.body);
-        return res.status(201).json({ message: 'Usuário cadastrado com sucesso', modelProduto: produto });
+        const produto = await Produto.create(req.body);
+        return res.status(201).json({ message: 'Usuário cadastrado com sucesso', Produto: produto });
     } catch (err) {
         return res.status(500).json({ error: err });
     }
 };
 async function index (req, res) {
     try {
-        const produto = await modelProduto.findAll();
+        const produto = await Produto.findAll();
         return res.status(200).json({ produto });
     } catch (err) {
         return res.status(500).json({ error:err })
@@ -19,7 +20,7 @@ async function index (req, res) {
 async function show (req, res) {
     const { id } = req.params;
     try {
-        const produto = await modelProduto.findByPk(id);
+        const produto = await Produto.findByPk(id);
         if (produto)
             return res.status(200).json({ produto });
         else
@@ -32,9 +33,9 @@ async function show (req, res) {
 async function update (req, res)  { 
     const {id} = req.params;
     try {
-        const updated = await modelProduto.update(req.body, {where: {id: id}});
+        const updated = await Produto.update(req.body, {where: {id: id}});
         if (updated) {
-            const produto = await modelProduto.findByPk(id);
+            const produto = await Produto.findByPk(id);
             res.status(200).send(produto)
         } else
             throw new Error();
@@ -45,7 +46,7 @@ async function update (req, res)  {
 async function destroy (req, res) { 
     const {id} = req.params;
     try {
-        const deleted = await modelProduto.destroy({where:{id:id}});
+        const deleted = await Produto.destroy({where:{id:id}});
         if (deleted) {
             return res.status(200).send('Usuário deletado com sucesso.')
         } else
@@ -55,10 +56,35 @@ async function destroy (req, res) {
     }
 };
 
+async function relVendedorProduto(req, res) {
+    const {vendedorId, produtoId} = req.params;
+    try {
+        const vendedor = await Vendedor.findByPk(vendedorId);
+        const produto = await Produto.findByPk(produtoId);
+        await produto.setVendedor(vendedor);
+        return res.status(200).json(produto);
+    } catch (error) {
+        return res.status(500).json({error})
+    }
+};
+
+async function RemoveRelVendedorProduto(req, res) {
+    const { produtoId } = req.params;
+    try {
+        const produto = await Produto.findByPk(produtoId);
+        await produto.setVendedor(null);
+        return res.status(200).json(produto);
+    } catch (error) {
+        return res.status(500).json({error})
+    }
+};
+
 module.exports = {
     create,
     index,
     show,
     update,
-    destroy
+    destroy,
+    relVendedorProduto,
+    RemoveRelVendedorProduto
 }
